cba07c98711366593ea38c9e3f3633431bb33af7
Alex Brown <doe.jimmie@gmail.com>
2021-06-21 03:45:47 -0500

    add auton selector

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,7 +6,9 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.commands.*;
 import frc.robot.subsystems.*;
@@ -47,29 +49,28 @@ public class RobotContainer {
   private final TowerSubsystem s_Tower = new TowerSubsystem();
   private final ShooterSubsystem s_Shooter = new ShooterSubsystem();
 
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  private SendableChooser<Command> autonSelector = new SendableChooser<>();
+
   public RobotContainer() {
     s_Drive.setDefaultCommand(new ArcadeDrive(s_Drive, driver, speedAxis, rotationAxis));
-
     configureButtonBindings();
+
+    /// ---- Autonomous Commands ---- ///
+    autonSelector.setDefaultOption("Do Nothing", new InstantCommand());
   }
 
   private void configureButtonBindings() {
+    /// ---- Driver Controls ---- ///
     intakeBalls.whenHeld(new IntakeBalls(s_Intake, s_Indexer, s_Tower));
 
+    /// ---- Operator Controls ---- ///
     unjam.whenHeld(new Unjam(s_Indexer, s_Tower));
     layupShot.whenHeld(new ManualShoot(s_Shooter, 3700, 76));
     midrangeShot.whenHeld(new ManualShoot(s_Shooter, 4200, 58));
     longShot.whenHeld(new ManualShoot(s_Shooter, 5600, 50));
   }
 
-  /**
-   * Use this to pass the autonomous command to the main {@link Robot} class.
-   *
-   * @return the command to run in autonomous
-   */
   public Command getAutonomousCommand() {
-    // An ExampleCommand will run in autonomous
-    return null;
+    return autonSelector.getSelected();
   }
 }
