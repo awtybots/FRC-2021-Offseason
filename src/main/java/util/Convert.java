554d15e73dc708820141d3ce3d55756a61f09327
Alex Brown <doe.jimmie@gmail.com>
2021-07-11 12:47:48 -0500

    extra conversion documentations

--- src/main/java/util/Convert.java
+++ src/main/java/util/Convert.java
@@ -6,10 +6,20 @@ package util;
 
 public class Convert {
 
+  /**
+   * @param ticks CANCoder Encoder Ticks
+   * @param gearRatio Gear Ratio between CANCoder and Mechanism
+   * @return Degrees of Rotation of Mechanism
+   */
   public static double canCoderToDegrees(double ticks, double gearRatio) {
     return ticks * (360.0 / (gearRatio * 4096.0));
   }
 
+  /**
+   * @param degrees Degrees of rotation of Mechanism
+   * @param gearRatio Gear Ratio between CANCoder and Mechanism
+   * @return Falcon Encoder Ticks
+   */
   public static double degreesToCanCoder(double degrees, double gearRatio) {
     return degrees / (360.0 / (gearRatio * 4096.0));
   }
@@ -33,12 +43,12 @@ public class Convert {
   }
 
   /**
-   * @param velocityCounts Falcon Encoder Ticks per 100ms
+   * @param velocity Falcon Velocity (ticks per 100ms)
    * @param gearRatio Gear Ratio between Falcon and Mechanism (set to 1 for Falcon RPM)
    * @return RPM of Mechanism
    */
-  public static double falconVelocityToRPM(double velocityCounts, double gearRatio) {
-    double motorRPM = velocityCounts * (600.0 / 2048.0);
+  public static double falconVelocityToRPM(double velocity, double gearRatio) {
+    double motorRPM = velocity * (600.0 / 2048.0);
     double mechRPM = motorRPM / gearRatio;
     return mechRPM;
   }
@@ -46,7 +56,7 @@ public class Convert {
   /**
    * @param RPM RPM of mechanism
    * @param gearRatio Gear Ratio between Falcon and Mechanism (set to 1 for Falcon RPM)
-   * @return RPM of Mechanism
+   * @return Falcon Velocity (ticks per 100ms) of Mechanism
    */
   public static double RPMToFalconVelocity(double RPM, double gearRatio) {
     double motorRPM = RPM * gearRatio;
